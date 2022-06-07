6a91df5e570c775dc9d014f2c779d74148fbc932
ferdi44443333333333333334 <97547021+ferdi44443333333333333334@users.noreply.github.com>
2022-06-07 15:25:04 +0300

    Add files via upload

---
+++ scrıpt.js
@@ -0,0 +1,10 @@
+function{
+           sayı=getelementbyıd.getAtiributte[+1];
+           sayı =getelementbyıd.getAtiributte[-1];
+               console.log(++1);
+}
+ function sayı{
+    let value =getAtiributte.getelementbyıd= dahakücükbirsayıgiriniz;
+    }
+    
+
